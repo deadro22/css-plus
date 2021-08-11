f91830112564a121ab8fd845dc97c98578b58b4e
unknown <gamemangamer8@gmail.com>
2021-08-11 18:50:22 +0100

    initial

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const Compiler = require("cssp-compiler");
-const source = fs.readFileSync(__dirname + "/src/style.cssp").toString();
 
+const source = fs.readFileSync(__dirname + "/src/style.cssp").toString();
 const output = Compiler.compile(source);
+
 fs.writeFileSync(__dirname + "/src/style.css", output);
